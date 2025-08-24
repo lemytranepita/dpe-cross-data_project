0bac8214a5ce22a89cea4c9b0dba79fe1d26e2d2
Le My TRAN <le-my.tran@epita.fr>
2025-08-24 16:50:59 +0200

    fix

--- filter_dvf.sh
+++ filter_dvf.sh
@@ -37,7 +37,6 @@ cat files/raw/dvf_2022.csv | grep \|Maison\| | grep \|Vente >> files/output/dvf_
 
 # Remove unneeded columns from CSV file.
 #   - First columns that don't contain any data
-
 awk -F'|' -v OFS='|' '{for(i=9;i<=NF;i++) printf "%s%s",$i,(i==NF?ORS:OFS)}' files/output/dvf_2024.csv > tmp.csv
 mv tmp.csv files/output/dvf_2024.csv
 
@@ -45,4 +44,14 @@ awk -F'|' -v OFS='|' '{for(i=9;i<=NF;i++) printf "%s%s",$i,(i==NF?ORS:OFS)}' fil
 mv tmp.csv files/output/dvf_2023.csv
 
 awk -F'|' -v OFS='|' '{for(i=9;i<=NF;i++) printf "%s%s",$i,(i==NF?ORS:OFS)}' files/output/dvf_2022.csv > tmp.csv
+mv tmp.csv files/output/dvf_2022.csv
+
+#   - Other unused columns
+awk -F'|' 'NR==1 {for (i=1; i<=NF; i++) if ($i ~ /Date mutation|Valeur fonciere|No voie|Type de voie|Voie|Code postal|Commune|Code departement|Code commune|Type local|Surface reelle bati|Nombre pieces principales/) cols[i]; for (i=1; i<=NF; i++) if (i in cols) { printf("%s%s",sep,$i); sep="|" }; print ""; next } NR>1 { sep=""; for (i=1; i<=NF; i++) if (i in cols) { printf("%s%s",sep,$i); sep="|" } print "" }' files/output/dvf_2024.csv > tmp.csv
+mv tmp.csv files/output/dvf_2024.csv
+
+awk -F'|' 'NR==1 {for (i=1; i<=NF; i++) if ($i ~ /Date mutation|Valeur fonciere|No voie|Type de voie|Voie|Code postal|Commune|Code departement|Code commune|Type local|Surface reelle bati|Nombre pieces principales/) cols[i]; for (i=1; i<=NF; i++) if (i in cols) { printf("%s%s",sep,$i); sep="|" }; print ""; next } NR>1 { sep=""; for (i=1; i<=NF; i++) if (i in cols) { printf("%s%s",sep,$i); sep="|" } print "" }' files/output/dvf_2023.csv > tmp.csv
+mv tmp.csv files/output/dvf_2023.csv
+
+awk -F'|' 'NR==1 {for (i=1; i<=NF; i++) if ($i ~ /Date mutation|Valeur fonciere|No voie|Type de voie|Voie|Code postal|Commune|Code departement|Code commune|Type local|Surface reelle bati|Nombre pieces principales/) cols[i]; for (i=1; i<=NF; i++) if (i in cols) { printf("%s%s",sep,$i); sep="|" }; print ""; next } NR>1 { sep=""; for (i=1; i<=NF; i++) if (i in cols) { printf("%s%s",sep,$i); sep="|" } print "" }' files/output/dvf_2022.csv > tmp.csv
 mv tmp.csv files/output/dvf_2022.csv
